73b69a20064eb93d3fd7487270c1a22d0b89f37d
Dmytro Tymchenko <timchenkod88@gmail.com>
2023-03-30 18:03:25 +0300

    fixed identifying currency rates

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
-import './App.css';
-import React from 'react';
-import Converter from './components/Converter';
+import "./App.css";
+import React from "react";
+import Converter from "./components/Converter";
 
 function App() {
-  const [date, setDate] = React.useState()
-  const [value1, setValue1] = React.useState(0)
-  const [value2, setValue2] = React.useState(0)
+  const [date, setDate] = React.useState();
+  const [value1, setValue1] = React.useState(0);
+  const [value2, setValue2] = React.useState(0);
 
   React.useEffect(() => {
     async function getData() {
-      const actualData = await fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
-      .then((response) => {
-        return response.json()
-      })
-      .catch((err) => {
-        console.warn(err)
-        alert("Not received info")
-      })
-      setValue1(actualData[25].rate)
-      setValue2(actualData[32].rate)
-      setDate(actualData[0].exchangedate)
+      const actualData = await fetch(
+        "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .catch((err) => {
+          console.warn(err);
+          alert("Not received info");
+        });
+      const findUSD = actualData.find(
+        (obj) => obj["cc"].toLowerCase() === "usd"
+      );
+      const findEUR = actualData.find(
+        (obj) => obj["cc"].toLowerCase() === "eur"
+      );
+      setValue1(findUSD.rate);
+      setValue2(findEUR.rate);
+      setDate(findUSD.exchangedate);
     }
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   const rates = {
     UAH: 1,
     USD: value1,
-    EUR: value2
-  }
-
+    EUR: value2,
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>Official Currency Rates by National Bank of Ukraine</h2>
         <h3>Stated for {date}</h3>
-        <div className='currency_rate_block'>
-          <div className='currency_rate_number'>{value1} UAH per 1 USD</div>
-          <div className='divider'></div>
-          <div className='currency_rate_number'>{value2} UAH per 1 EUR</div>
+        <div className="currency_rate_block">
+          <div className="currency_rate_number">{value1} UAH per 1 USD</div>
+          <div className="divider"></div>
+          <div className="currency_rate_number">{value2} UAH per 1 EUR</div>
         </div>
       </header>
       <Converter value1={value1} value2={value2} rates={rates} />
